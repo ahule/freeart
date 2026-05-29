c30e76884656212c05e429f1f79ea9e82ad523ca
ahule <ahmadsadaqa027@gmail.com>
2026-05-29 14:07:49 +0000

    server core v2, a lot of security updates and improving server speed and reliability

--- client.cpp
+++ client.cpp
@@ -6,37 +6,45 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <string>
+
 #include "fcp.h"
 
 using namespace std;
 
-int login = false;
+bool login_flag = false;
 string SERVER_IP = "127.0.0.1";
 
 void recv_chat(int sockfd) {
     while (true) {
         packethdr header;
+
         char* buffer = recv_packet(sockfd, header);
         if (buffer == nullptr) {
-            continue;
+            cout << "\n[Disconnected] Lost connection to server or invalid packet received." << endl;
+            close(sockfd);
+            exit(0);
         }
-        
-        cout << "\r[" << header.sender << "]: " << buffer << "\nclient: " << flush;
+
+        string sender_name(header.sender);
+        if (sender_name.empty()) {
+            sender_name = "Anonymous";
+        }
+
+        cout << "\r[" << sender_name << "]: " << buffer << "\nclient: " << flush;
+
         delete[] buffer;
     }
 }
 
 int main() {
-    // create socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd != -1) {
         cout << "Create Socket Success." << endl;
-    }else {
+    } else {
         cout << "Socket Creation Failed." << endl;
         return 1;
     }
 
-    // connect the client to server
     struct sockaddr_in sock_addr;
     memset(&sock_addr, 0, sizeof(sock_addr));
 
@@ -46,47 +54,50 @@ int main() {
 
     if (connect(sockfd, (struct sockaddr*) &sock_addr, sizeof(sock_addr)) != -1) {
         cout << "Connect Success." << endl;
-    }else {
+    } else {
         cout << "Connect Failed." << endl;
         return 1;
     }
 
-    // login
     while (true) {
         string msg = "";
-        cout << "Login: ";
+        cout << "Login Password: ";
         getline(cin, msg);
         if (msg == "exit") {
-            break;
+            close(sockfd);
+            return 0;
         }
 
         send_packet(sockfd, 1, msg);
 
         int login_status = 0;
-        int login_recv = recv(sockfd, &login_status, sizeof(login_status), 0);
-        if (login_recv == sizeof(login_status)) {
+        // استخدام recv_all لضمان قراءة الـ 4 بايت كاملة دون تقطيع بالشبكة
+        if (recv_all(sockfd, &login_status, sizeof(login_status))) {
             if (login_status == -1) {
                 cout << "Login Failed. Try again." << endl;
             } else {
                 cout << "Login Success!" << endl;
-                login = true;
+                login_flag = true;
                 break;
             }
         } else {
             cout << "Connection error during login." << endl;
-            break;
+            close(sockfd);
+            return 1;
         }
     }
 
-    // send messages
-    if (login) {
+    if (login_flag) {
         thread(recv_chat, sockfd).detach();
     }
-    while (login) {
+
+    while (login_flag) {
         string msg = "";
         cout << "client: " << flush;
         getline(cin, msg);
         if (msg.empty()) continue;
+
+        if (msg == "exit") break;
 
         send_packet(sockfd, 2, msg);
     }
--- fcp.h
+++ fcp.h
@@ -1,6 +1,13 @@
 #ifndef FREEART_FCP_H
 #define FREEART_FCP_H
 
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cerrno>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 using namespace std;
 
@@ -9,13 +16,28 @@ struct packethdr { // FCP : Freeart Chat Protocol
     int type;   // 1: Login, 2: Message, 3: Exit
     int length;
     char magic[3];
-    char version[3]; // "1.01"
+    char version[3]; // "1.0"
     char sender[16];
     unsigned int checksum = 0;
 };
 #pragma pack(pop)
 
-unsigned int checksum(const void* data, size_t length) {
+struct client_session {
+    int fd;
+    vector<char> headerbuf;
+    vector<char> bodybuf;
+    packethdr header;
+    bool header_ready = false;
+};
+
+inline void set_nonblocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags != -1) {
+        fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+    }
+}
+
+inline unsigned int checksum(const void* data, size_t length) {
     const uint16_t* ptr = static_cast<const uint16_t*>(data);
     uint32_t sum = 0;
 
@@ -33,7 +55,7 @@ unsigned int checksum(const void* data, size_t length) {
     return static_cast<uint16_t>(~sum);
 }
 
-bool recv_all(int socket, void* buffer, size_t length) {
+inline bool recv_all(int socket, void* buffer, size_t length) {
     char* ptr = static_cast<char*>(buffer);
     while (length > 0) {
         int bytes = recv(socket, ptr, length, 0);
@@ -44,12 +66,12 @@ bool recv_all(int socket, void* buffer, size_t length) {
     return true;
 }
 
-void send_packet(int sockfd, int packet_type, const string& message) {
+inline void send_packet(int sockfd, int packet_type, const string& message) {
     packethdr header;
     memset(&header, 0, sizeof(header));
 
     header.type = packet_type;
-    header.length = strlen(message.c_str());
+    header.length = message.length();
     strncpy(header.magic, "FCP", 3);
     strncpy(header.version, "1.0", 3);
 
@@ -70,38 +92,98 @@ void send_packet(int sockfd, int packet_type, const string& message) {
     delete[] sendbuf;
 }
 
-char* recv_packet(int sockfd, packethdr& header) {
+inline char* recv_packet(int sockfd, packethdr& header) {
     if (!recv_all(sockfd, &header, sizeof(header))) {
-        cout << "\n[Connection] Server disconnected." << endl;
-        close(sockfd);
         return nullptr;
     }
-
     if (header.magic[0] != 'F' || header.magic[1] != 'C' || header.magic[2] != 'P') {
         return nullptr;
     }
-
     if (header.length > 0) {
         char* buffer = new char[header.length + 1];
-
         if (recv_all(sockfd, buffer, header.length)) {
             buffer[header.length] = '\0';
-
             if (header.checksum == checksum(buffer, header.length)) {
                 return buffer;
-            } else {
-                cout << "\n[Security Error] Packet corrupted! Checksum mismatch." << endl;
-                delete[] buffer;
-                return nullptr;
             }
-        } else {
             delete[] buffer;
-            return nullptr;
         }
     }
     return nullptr;
 }
 
+inline int recv_packet_nonblocking(client_session& session, char*& out_buffer) {
+    out_buffer = nullptr;
 
+    if (!session.header_ready) {
+        size_t current_size = session.headerbuf.size();
+        size_t bytes_needed = sizeof(packethdr) - current_size;
+
+        vector<char> temp_buf(bytes_needed);
+        int bytes_recv = recv(session.fd, temp_buf.data(), bytes_needed, 0);
+
+        if (bytes_recv > 0) {
+            session.headerbuf.insert(session.headerbuf.end(), temp_buf.begin(), temp_buf.begin() + bytes_recv);
+
+            if (session.headerbuf.size() == sizeof(packethdr)) {
+                memcpy(&session.header, session.headerbuf.data(), sizeof(packethdr));
+
+                if (session.header.magic[0] != 'F' || session.header.magic[1] != 'C' || session.header.magic[2] != 'P') {
+                    return -1;
+                }
+                session.header_ready = true;
+            }
+        }
+        else if (bytes_recv == 0) {
+            return -1;
+        }
+        else {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                return 0;
+            }
+            return -1;
+        }
+    }
+
+    if (session.header_ready) {
+        if (session.header.length == 0) {
+            out_buffer = new char[1];
+            out_buffer[0] = '\0';
+            return 1;
+        }
+
+        size_t current_body_size = session.bodybuf.size();
+        size_t body_bytes_needed = session.header.length - current_body_size;
+
+        vector<char> temp_body(body_bytes_needed);
+        int bytes_recv = recv(session.fd, temp_body.data(), body_bytes_needed, 0);
+
+        if (bytes_recv > 0) {
+            session.bodybuf.insert(session.bodybuf.end(), temp_body.begin(), temp_body.begin() + bytes_recv);
+        }
+        else if (bytes_recv < 0 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+
+        if (session.bodybuf.size() == session.header.length) {
+            out_buffer = new char[session.header.length + 1];
+            memcpy(out_buffer, session.bodybuf.data(), session.header.length);
+            out_buffer[session.header.length] = '\0';
+
+            if (session.header.checksum == checksum(out_buffer, session.header.length)) {
+                return 1;
+            } else {
+                delete[] out_buffer;
+                out_buffer = nullptr;
+                return -1;
+            }
+        }
+    }
+
+    return 0;
+}
 
 #endif //FREEART_FCP_H
